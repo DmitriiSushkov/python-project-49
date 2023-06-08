c6714cbf59bf8d7493ece6fac209ef600d203f1d
Dmitrii Sushkov <python.dev.cat@gmail.com>
2023-06-08 20:10:16 +0300

    3 lesons

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python3
-def main():
+from brain_games.cli import welcome_user
+
+
+
+def welcom():
     print('Welcome to the Brain Games!')
+
+def main():
+    welcom()
+    welcome_user()
+
+
+if __name__ == '__main__':
+    main()
